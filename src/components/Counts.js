f21ccea64d2e74cc7f654d0e742c47435a4f9f20
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-25 18:40:05 +0600

    adding increment decrement restart button

--- src/components/Counts.js
+++ src/components/Counts.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux/es/exports';
+import { useDispatch } from 'react-redux/es/hooks/useDispatch';
+import { decreamentCounter, increamentCounter, resetCounter } from '../services/actions/counterAction';
 
 const Counts = () => {
+    const count = useSelector((state)=> state.count) //import from redux to find the state
+    const  dispatch = useDispatch(); //import from redux to get dispatch
+    const handleIncrement = () =>{
+        dispatch(increamentCounter())
+    }
+    const handleReset = () =>{
+        dispatch(resetCounter())
+    }
+    const handleDecrement = () =>{
+        dispatch(decreamentCounter())
+    }
     return (
         <div>
-            <h2>This is count app</h2>
+            <h2>This is count app {count}</h2>
+            <button onClick={handleIncrement}>incremane</button>
+            <button onClick={handleReset}>reset</button>
+            <button onClick={handleDecrement}>decrement</button>
         </div>
     );
 };
